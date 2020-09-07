c7b2b2a4db867979bc7ae5375581073eb4e08e2c
majcen-dev <timotej.majcen@gmail.com>
2020-09-07 05:28:04 +0200

    whoopsie info

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -64,6 +64,12 @@ function Navbar(props) {
               </div>
             </>}
 
+          {!props.owner &&
+            <div onClick={() => { window.open("https://github.com/majcen-dev/bggs/blob/master/README.md", "_blank"); }} className="cursor-pointer inline">{/* INFO */}
+              <svg viewBox="0 0 20 20" fill="currentColor" className="information-circle w-6 h-6 mx-3 inline" style={{ fill: "#fff" }} ><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path></svg>
+            </div>
+          }
+
         </div>
 
       </nav>
